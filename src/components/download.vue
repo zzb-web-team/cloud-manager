a4bd9f75db25c4f516369e9f5978fa9390a2f361
xionghao <2576181740@qq.com>
2020-11-25 14:30:34 +0800

    导出

---
+++ src/components/download.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="exportWrapper">
+    <div class="wrapper" v-show="showProgress">
+        <div class="triangel"></div>
+        <img src="../assets/img/excel.png" alt="">
+        <div class="progress">
+            <div class="progress"  :style="{width: percentage + '%', background: '#297AFF', margin: 0}"></div>
+        </div>
+        <p v-if="percentage<100">{{percentage}}%</p>
+        <img v-else src="../assets/img/complete.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pagination",
+  data() {
+    return {
+        percentage: 0,
+        fileSize: 0,
+        showProgress: false, 
+    };
+  },
+  methods: {
+    download(url, filename) {
+      this.showProgress = true;
+    //   this.get_filesize(url);
+      this.getBlob(url).then((blob) => {
+        console.log(blob);
+        this.saveAs(blob, filename);
+      });
+    },
+    get_filesize(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("HEAD",url,true); // Notice "HEAD" instead of "GET",//  to get only the header
+        xhr.onreadystatechange = function() {
+            if (this.readyState == this.DONE) {
+                let fileSize = xhr.getResponseHeader("Content-Length");
+                this.fileSize = fileSize;
+            }
+        };
+        xhr.send();
+    },
+
+    getBlob(url) {
+      return new Promise((resolve) => {
+        let that = this; // 创建XMLHttpRequest，会让this指向XMLHttpRequest，所以先接收一下this
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("GET", url, true);
+
+        //监听进度事件
+        xhr.addEventListener(
+          "progress",
+          function (evt) {
+            console.log(evt);
+            let fileSize = xhr.getResponseHeader("Content-Length");
+            console.log(fileSize)
+            let percentComplete = evt.loaded / evt.total;
+            that.percentage = percentComplete * 100;
+          },
+          false
+        );
+
+        xhr.responseType = "blob";
+        xhr.onload = () => {
+          if (xhr.status === 200) {
+            resolve(xhr.response);
+          }
+        };
+        xhr.send();
+      });
+    },
+
+    saveAs(blob, filename) {
+      // ie的下载
+      console.log(blob, filename);
+      this.showProgress = false;
+      this.percentage = 0;
+      this.fileSize = 0;
+      if (window.navigator.msSaveOrOpenBlob) {
+        navigator.msSaveBlob(blob, filename);
+      } else {
+        console.log("aaaaaa");
+        // 非ie的下载
+        const link = document.createElement("a");
+        const body = document.querySelector("body");
+
+        link.href = window.URL.createObjectURL(blob);
+        link.download = filename;
+
+        // fix Firefox
+        link.style.display = "none";
+        body.appendChild(link);
+
+        link.click();
+        body.removeChild(link);
+
+        window.URL.revokeObjectURL(link.href);
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.exportWrapper{
+    display: flex;
+    flex-direction: column;
+    // justify-content: ;
+    align-items: flex-end;
+    position: relative;
+    .wrapper{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-start;
+        padding:25px 15px;
+        background: #fff;
+        border: 1px solid #eee;
+        border-radius: 8px;
+        width: 200px;
+        position: absolute;
+        right: -60px;
+        top: 60px;
+        z-index: 10;
+        .progress{
+            width: 100px;
+            height: 4px;
+            border-radius: 4px;
+            background: #DFECFD;
+            margin: 0 8px;
+        }
+    }
+    .wrapper:before,.wrapper:after{
+        position: absolute;
+        content: " ";
+        top: -16px;
+        left: 100px;
+        display: block;
+        width: 0;
+        height: 0;
+        border: 8px solid #fff;
+        border-color: transparent transparent #fff transparent;
+        z-index: 111;
+    }
+    .wrapper:after{
+        top: -17px;
+        border-color: transparent transparent #eee transparent;
+        z-index: 11;
+    }
+}
+</style>
--- src/views/statistics/node-traffic-monitoring.vue
+++ src/views/statistics/node-traffic-monitoring.vue
@@ -136,6 +136,7 @@
               </el-radio-group>
               <div style="display: flex;justify-content: flex-end;margin-right: 6px;">
                 <el-button type="primary" @click="toExportExcel">导出</el-button>
+                <Download ref="download" />
               </div>
               <el-row v-show="radioTop == 1" type="flex" class="row_active">
                 <el-col :span="24">
@@ -224,6 +225,7 @@
 <script>
 import { dateToMs, getymdtime, getymdtime1, splitTimes } from "../../servers/sevdate";
 import fenye from "@/components/fenye";
+import Download from "@/components/download";
 import {
   node_traffic_curve,
   node_traffic_table,
@@ -359,6 +361,7 @@ export default {
   },
   components: {
     fenye,
+    Download
   },
   mounted() {
     let monitorUrlname = this.$route.query.monitorUrlname;
@@ -844,64 +847,6 @@ export default {
         });
     },
 
-    download(url, filename) {
-      this.getBlob(url).then(blob => {
-        this.saveAs(blob, filename);
-      });
-    },
-    getBlob(url) {
-      return new Promise(resolve => {
-          let that = this; // 创建XMLHttpRequest，会让this指向XMLHttpRequest，所以先接收一下this
-          const xhr = new XMLHttpRequest();
-
-          xhr.open("GET", url, true);
-
-          //监听进度事件
-          xhr.addEventListener(
-              "progress",
-              function(evt) {
-                  if (evt.lengthComputable) {
-                      let percentComplete = evt.loaded / evt.total;
-                      that.percentage = percentComplete * 100;
-                  }
-              },
-              false
-          );
-
-          xhr.responseType = "blob";
-          xhr.onload = () => {
-              if (xhr.status === 200) {
-                  resolve(xhr.response);
-              }
-          };
-          
-          xhr.send();
-      });
-    },
-
-    saveAs(blob,  string) {
-    // ie的下载
-      if (window.navigator.msSaveOrOpenBlob) {
-          navigator.msSaveBlob(blob, filename);
-      } else {
-          // 非ie的下载
-          const link = document.createElement("a");
-          const body = document.querySelector("body");
-
-          link.href = window.URL.createObjectURL(blob);
-          link.download = filename;
-
-          // fix Firefox
-          link.style.display = "none";
-          body.appendChild(link);
-
-          link.click();
-          body.removeChild(link);
-
-          window.URL.revokeObjectURL(link.href);
-      }
-    },
-
     toExportDataflowExcel() {
       let params = new Object();
       params.startTs = this.starttime;
@@ -929,7 +874,9 @@ export default {
       );
       export_dataflow_ranking_table_file(params).then(res => {
           if (res.status == 0) {
-            window.open(res.msg, "_blank");
+            // window.open(res.msg, "_blank");
+            this.$refs.download.download(res.msg, 'export_dataflow_ranking_table_file-20201124.csv')
+            // this.download(res.msg, 'export_dataflow_ranking_table_file-20201124.csv')
           }
         })
         .catch(error => {
